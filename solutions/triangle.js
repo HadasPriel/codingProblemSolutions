34ea9905bad7276e9aa9426bb6cb2671a213fb5d
hadas <hadass91@gmail.com>
2023-09-25 14:38:31 +0300

    solution 1: triangle

--- solutions/triangle.js
+++ solutions/triangle.js
@@ -1,4 +1,5 @@
 'use strict'
+
 // Given a triangle array, return the minimum path sum from top to bottom.
 
 // For each step, you may move to an adjacent number of the row below. 
@@ -22,12 +23,33 @@
  */
 
 function minimumTotal(triangle) {
-
+    triangle = JSON.parse(JSON.stringify(triangle))
+    for (let i = triangle.length - 2; i >= 0; i--) {
+        for (let j = 0; j < triangle[i].length; j++) {
+            triangle[i][j] += Math.min(triangle[i + 1][j], triangle[i + 1][j + 1])
+        }
+    }
+    return triangle[0][0]
 }
 
 
 
 var triangle = [
+    [2],
+    [3, 4],
+    [6, 5, 7],
+    [4, 1, 8, 3]
+]
+
+console.log('INPUT:', triangle)
+console.log('EXPECTED: ', 11)
+var res = minimumTotal(triangle)
+console.log('ACTUAL: ', res)
+
+
+
+
+triangle = [
     [2],
     [3, 1],
     [6, 5, 1],
@@ -36,5 +58,8 @@ var triangle = [
 
 console.log('INPUT:', triangle)
 console.log('EXPECTED: ', 7)
-var res = minimumTotal(triangle)
+res = minimumTotal(triangle)
 console.log('ACTUAL: ', res)
+
+
+
